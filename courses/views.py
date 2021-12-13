25bb17fdf8457a2c078d94a944937b33e89c8933
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-14 00:22:03 +0300

    fix exception for not logged in users

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,7 @@ def courses(request, category_slug=None, tag_slug=None):
     tag_page = None
     category_list = Category.objects.all()
     tag_list = Tag.objects.all()
+    current_user = request.user
 
     if category_slug != None:
         # first param is our Category model, second one is the slug field of Category model
@@ -18,7 +19,14 @@ def courses(request, category_slug=None, tag_slug=None):
         tag_page = get_object_or_404(Tag, slug=tag_slug)
         courses = Course.objects.filter(available=True, tags=tag_page)
     else:
-        courses = Course.objects.all().order_by('-date')
+        if current_user.is_authenticated:
+            enrolled_courses = current_user.joined_courses.all()
+            courses = Course.objects.all().order_by('-date')
+            for course in enrolled_courses:
+                courses = courses.exclude(id=course.id)
+        else:
+            courses = Course.objects.all().order_by('-date')
+
     
     context = {
         'courses': courses,
@@ -30,11 +38,24 @@ def courses(request, category_slug=None, tag_slug=None):
 
 
 def course_detail(request, category_slug, course_id):
+    current_user = request.user
     course = Course.objects.get(category__slug = category_slug, id = course_id)
+    courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    if current_user.is_authenticated:
+        enrolled_courses = current_user.joined_courses.all()
+    else:
+        enrolled_courses = courses
+
 
     context = {
         'course': course,
+        'enrolled_courses':enrolled_courses,
+        'categories':categories,
+        'tags': tags,
     }
+    
     return render(request, 'course.html', context)
 
 
