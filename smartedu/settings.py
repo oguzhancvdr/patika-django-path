dbf87c0f1274ecd34dd514227e1f1ba9b31388b2
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-14 16:21:09 +0300

    add white noise middleware

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [str(BASE_DIR / 'static'),]
+
 # MEDIA FILES
 
 MEDIA_URL = '/media/'
