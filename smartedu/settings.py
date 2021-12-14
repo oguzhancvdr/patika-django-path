827998f9215b455c1d74abb6845bd6ea9adc3fd7
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-14 16:59:07 +0300

    fix build err

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'static')
 STATICFILES_DIRS = [str(BASE_DIR / 'static'),]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA FILES
 
